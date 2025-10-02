8fdd7e946514a268fc4b49c48d855d0397f26221
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-10-02 11:25:41 -0300

    feat: add MongooseModule for MongoDB connection in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.2dzfduq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
